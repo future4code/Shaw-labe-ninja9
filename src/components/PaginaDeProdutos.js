9a1e06a588fa950815fd463063c057231307bcd1
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-11 14:53:47 -0300

    Esqueleto do site feito em grupo

---
+++ src/components/PaginaDeProdutos.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components"
+import axios from 'axios';
+import Header from './Header'
+
+export default class PaginadeProdutos extends React.Component {
+  render() {
+
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+
+  }
+}
