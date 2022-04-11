1d002e3750677f3adf470052dba7af555852f919
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-11 18:07:47 -0300

    Layout inicial da página home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios';
 import styled from "styled-components"
 import Header from './Header'
+import Home from './components/Home';
 
 
 export default class App extends React.Component {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import axios from 'axios'
 import styled from "styled-components"
-import Header from './Header'
 
 export default class Header extends React.Component {
   render() {
