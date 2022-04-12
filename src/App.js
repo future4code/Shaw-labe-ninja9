c9281f2c9e789778169fbbb2ad6dd46ce3170069
Regiscleia Dias Amorim Silva <regiscleiaa@gmail.com>
2022-04-12 09:49:36 -0300

    pagina cadastro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios';
 import styled from "styled-components"
 import Header from './Header'
+import Cadastro from './components/Cadastro';
 
 
 export default class App extends React.Component {
--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,15 +1,72 @@
-import React from 'react'
-import axios from 'axios'
-import styled from "styled-components"
-import Header from './Header'
+import React from 'react';
+import axios from 'axios';
+import styled from "styled-components";
+//import Calendar from 'react-calendar';
+
 
 export default class Cadastro extends React.Component {
+
+state={
+  titulo:"",
+  descrição:"",
+  preco:"",
+  pagamento:"",
+  data:""
+};
+
+
+
+onChangeInputTitulo=(e)=>{
+this.setState({titulo: e.target.value})
+}
+onChangeInputDescricao =()=>{
+
+}
+
+onchangeinputPreco =()=>{
+  
+};
+
+
+
   render() {
 
     return (
-          <div>
-              <Header/>
-          </div>
+      <div>
+        <div>
+          <header/>
+        </div>
+        <h1>Cadastre seu Serviço</h1>
+        <input
+          placeholder='Titulo do Serviço'
+          
+        />
+        <input
+          placeholder='Descrição'
+          
+        
+        />
+        <input
+          placeholder='Preço'
+          
+          
+        />
+        <select multiple>
+          <option>Cartão de credito </option>
+          <option>Cartão de debito </option>
+          <option>Pix </option>
+          <option>Boleto bancario</option>
+          <option>paypal </option>
+
+        </select>
+        <div>
+          <button>Cadastrar</button>
+        </div>
+
+      
+
+      </div>
+
     )
 
   }
