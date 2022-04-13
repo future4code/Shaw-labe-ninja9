41fee6855bf158d0f8fa0cd7c21da31f84e68c78
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-13 11:15:53 -0300

    card de serviço criado e filtros colocados na esquerda

--- src/components/PaginaDeProdutos.js
+++ src/components/PaginaDeProdutos.js
@@ -1,48 +1,104 @@
 import React from 'react'
 import styled from "styled-components"
-import axios from 'axios';
-import Header from './Header'
+import axios from 'axios'
+import {urlBase, headers} from '../constants/variaveisApi'
 import Cadastro from "./Cadastro"
 import PaginaDeProdutos from "./PaginaDeProdutos"
 
-const InputContainer = styled.div`
-padding: 10px;
-display: flex;
-justify-content:space-around;
+const ContainerPrincipal = styled.div`
+  display: flex;
+`
+
+const ContainerFiltros = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 10px;
+  border: 1px solid black;
+`
+
+const ContainerProdutos = styled.div`
+  display: flex;
+  padding: 10px;
+`
+
+const ContainerCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  height: 250px;
 `
 
 
-
-
 export default class PaginadeProdutos extends React.Component {
+
+  state = {
+    servicos: []
+  }
+
+  componentDidMount() {
+    this.mostrarTodosOsServicos()
+  }
+
+  mostrarTodosOsServicos = () => {
+    axios
+    .get(`${urlBase}/jobs`, headers)
+    .then((res) => {
+      this.setState({
+        servicos: res.data.jobs
+      })
+    })
+    .catch((err) => console.log(err.response))
+  }
+
   render() {
+    console.log(this.state.servicos)
+
+    const listaDeServicos = this.state.servicos.map((servico) => {
+      return (
+        <ContainerCard>
+          <h3 key={servico.id}>{servico.title}</h3>
+          <p>Preço: {servico.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+          <p>Prazo: {servico.dueDate}</p>
+
+          <div>
+            <button>Detalhes</button>
+            <button>Adicionar ao carrinho </button>
+          </div>
+        </ContainerCard>
+      )
+    })
 
     return (
-      <div>
-        {/* <Header/> */}
-        <InputContainer>
-        <input
-          placeholder='Valor Mínimo'
-          type='Number'
-        />
-        <input
-          placeholder='Valor Maximo'
-          type='Number'
-        />
-        <input
-          placeholder='Buscar por titulo ou descrição'
-          type='text'
-        />
-        <select name="Sem ordenação">
-        <option value="valor1">Sem ordenação</option>
-          <option value="valor2">Menor Valor</option>
-          <option value="valor3" >Maior Valor</option>
-          <option value="valor4">Titulo</option>
-          <option value="valor5">Prazo</option>
-        </select>
-        </InputContainer>
-        
-      </div>
+      <ContainerPrincipal>
+
+        <ContainerFiltros>
+          <input
+            placeholder='Valor Mínimo'
+            type='Number'
+          />
+          <input
+            placeholder='Valor Maximo'
+            type='Number'
+          />
+          <input
+            placeholder='Buscar por titulo ou descrição'
+            type='text'
+          />
+          <select name="Sem ordenação">
+            <option value="valor1">Sem ordenação</option>
+            <option value="valor2">Menor Valor</option>
+            <option value="valor3" >Maior Valor</option>
+            <option value="valor4">Titulo</option>
+            <option value="valor5">Prazo</option>
+          </select>
+        </ContainerFiltros>
+
+        <ContainerProdutos>
+          {listaDeServicos}
+        </ContainerProdutos>
+
+      </ContainerPrincipal>
     )
 
   }
