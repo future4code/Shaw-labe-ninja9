cc2bcf6894d2aec879b80382b625ca1c59e3f4b4
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2022-04-14 20:04:02 -0300

    segunda tentativa da estilizacao do carrinho

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,40 @@ import Home from './Home'
 import Carrinho from './Carrinho'
 
 const MainContainer = styled.div`
-  border: 1px solid black;
-  background-color: lightseagreen;
+  background-color: #7165bf;
+  color: white;
   display: flex;
   justify-content: space-between;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 `
 
-const Cabecalho = styled.div`
+const AreaDosBotoes = styled.div`
   padding: 25px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
 `
+
+const Titulo = styled.p`
+  padding: 25px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  font-size: 20px;
+`
+const Botao = styled.button`
+  background-color: #7165bf;
+  color: white;
+  font-size: 18px;
+  border: 0;
+  margin: 20px;
+  padding: 8px 16px;
+  &:hover{
+    background-color: #9f96d9;
+  }
+`
+
+
 
 
 export default class Header extends React.Component {
@@ -22,13 +47,11 @@ export default class Header extends React.Component {
 
     return (
       <MainContainer>
-        <Cabecalho>
-          Nome do Projeto
-        </Cabecalho>
-        <Cabecalho>
-          <button onClick={this.props.irParaHome}>Home</button>
-          <button onClick={this.props.irParaCarrinho}>Carrinho</button>
-        </Cabecalho>
+        <Titulo><b>LabeNinjas</b></Titulo>
+        <AreaDosBotoes>
+          <Botao onClick={this.props.irParaHome} style={{cursor:'pointer'}}>Home</Botao>
+          <Botao onClick={this.props.irParaCarrinho} style={{cursor:'pointer'}}>Carrinho</Botao>
+        </AreaDosBotoes>
       </MainContainer>
     )
 
